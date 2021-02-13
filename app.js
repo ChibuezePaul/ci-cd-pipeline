7b4349efccf61d76ab464b33d50c9e77dd73c066
ChibuezePaul <ChibuezePaul2@gmail.com>
2021-02-13 12:55:31 +0100

    PORT FIXED

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const port = process.env.port;
+const port = process.env.PORT;
 http
 .createServer((req, res) => res.end("Hello CI/CI Pipeline..."))
 .listen(port, () => console.log(`CI/CD Pipleline Started On ${port}...`));
