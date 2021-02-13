3f07cd5172a196f8a43b9d524894bce4c22452be
ChibuezePaul <ChibuezePaul2@gmail.com>
2021-02-13 13:19:45 +0100

    port conf for local

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const port = process.env.PORT;
+const port = process.env.PORT || 3500;
 http
 .createServer((req, res) => res.end("CI/CD Pipeline Worked..."))
 .listen(port, () => console.log(`CI/CD Pipleline Started On ${port}...`));
